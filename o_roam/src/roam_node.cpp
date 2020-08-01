78f834535a8f175267ac259ad5d74be09b0c84fb
Michael Wimble <mike@wimblerobotics.com>
2020-07-31 22:30:53 -0700

    roam_node now finds closest point in local cost map

--- o_roam/src/roam_node.cpp
+++ o_roam/src/roam_node.cpp
@@ -76,22 +76,8 @@ void laserCallback(const sensor_msgs::LaserScanConstPtr& laser_msg) {
 }
 
 
-// some definitions of functions
-//#define MAP_WXGX(map, i) (origin_x + (i - map->info.width / 2) * map->info.resolution)
-//#define MAP_WYGY(map, j) (origin_y + (j - map->info.height / 2) * map->info.resolution)
 inline int mapIndex(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg, int i, int j) {
     int result = i + (j * costmap_msg->info.width);
-    return result;
-}
-
-inline double mapXtoGlobalX(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg, int map_x) {
-//    double result = origin_x + (map_x - (costmap_msg->info.width / 2)) * costmap_msg->info.resolution;
-    double result = costmap_msg->info.origin.position.x + (map_x * costmap_msg->info.resolution);
-    return result;
-}
-
-inline double mapYtoGlobalY(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg, int map_y) {
-    double result = costmap_msg->info.origin.position.x + (map_y * costmap_msg->info.resolution);
     return result;
 }
 
@@ -107,13 +93,10 @@ void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg) {
 			 costmap_msg->info.origin.position.y, 
 			 costmap_msg->info.origin.position.z);
 
-    double dist_sq;
-    double d_x = -10000;
-    double d_y = -10000;
+    std::size_t d_x = -10000;
+    std::size_t d_y = -10000;
     double min_dist = 10000;
     geometry_msgs::PoseStamped new_pt;
-    double origin_x;
-    double origin_y;
     geometry_msgs::PoseStamped pt;
 
     pt.header.frame_id = "base_link";
@@ -124,9 +107,6 @@ void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg) {
     pt.pose.orientation.x = 0;
     pt.pose.orientation.y = 0;
     pt.pose.orientation.z = 0;
-
-    origin_x = costmap_msg->info.origin.position.x + ((costmap_msg->info.width / 2) * costmap_msg->info.resolution);
-    origin_y = costmap_msg->info.origin.position.y + ((costmap_msg->info.height / 2) * costmap_msg->info.resolution);
 
     tf::TransformListener tf_l(ros::Duration(10));
 
@@ -152,38 +132,20 @@ void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg) {
             double cost = costmap_msg->data[mapIndex(costmap_msg, i, j)];
             if(cost == 100) {
                 // convert to world position
-                double w_x = mapXtoGlobalX(costmap_msg, i);
-                double w_y = mapYtoGlobalY(costmap_msg, j);
-                double dist_sq = pow(w_x - origin_x, 2) + pow(w_y - origin_y, 2);
-                double dist = sqrt(dist_sq);
                 double d2_sq = ((i - i_midpoint) * (i - i_midpoint)) + ((j - j_midpoint) * (j - j_midpoint));
-                double d2_dist = sqrt(d2_sq * 1.0);
-                if (dist_sq < 1.0) {
+                double d2_dist = sqrt(d2_sq * 1.0) * costmap_msg->info.resolution;
+                if (d2_dist < 1.0) {
                     ROS_INFO("[roam_node] i: %ld, j: %ld"
-                             ", w_x: %7.4f, w_y: %7.4f"
-                             ", origin_x: %7.4f, origin_y: %7.4f"
-                             ", dist_sq: %7.4f, dist: %7.4f, min_dist: %7.4f"
+                             ", d2_sq: %7.4f, d2_dist: %7.4f"
+                             ", old min_dist: %7.4f"
                              ", new min: %s"
                              , i, j
-                             , w_x, w_y
-                             , origin_x, origin_y
-                             , dist_sq, dist, min_dist
-                             , (dist_sq < min_dist) ? "TRUE" : "false"
-                    );
-                    ROS_INFO("[roam_node] "
-                             "w_x - origin_x: %7.4f"
-                             ", pow(w_x - origin_x, 2): %7.4f"
-                             ", w_y - origin_y: %7.4f"
-                             ", pow(w_y - origin_y, 2): %7.4f"
-                             ", d2_sq: %7.4f, d2_dist: %7.4f"
-                             , w_x - origin_x
-                             , pow(w_x - origin_x, 2)
-                             , w_y - origin_y
-                             , pow(w_y - origin_y, 2)
-                             , d2_sq, d2_dist * costmap_msg->info.resolution
+                             , d2_sq, d2_dist
+                             , min_dist
+                             , (d2_dist < min_dist) ? "TRUE" : "false"
                     );
                 }
-                if (dist_sq < min_dist) {
+                if (d2_dist < min_dist) {
                     for (std::size_t jx = costmap_msg->info.height; jx > 0 ; jx--) {
                         char line[costmap_msg->info.width];
                         for (std::size_t ix = 0; ix < costmap_msg->info.width; ix++) {
@@ -195,24 +157,29 @@ void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap_msg) {
                         if (jx == j) line[i] = 'X';
                         ROS_INFO("%c %3ld: %s", (jx==j)? '>' : ' ', jx, line);
                     }
-                    min_dist = dist_sq;
-                    d_x = w_x;
-                    d_y = w_y;
+                    min_dist = d2_dist;
+                    d_x = i;
+                    d_y = j;
                 }
             }
         }
     }
 
-    double angle = std::atan2(d_y - pt_y, d_x - pt_x) - pt_th;
+    double angle = std::atan2(d_y - (j_midpoint * 1.0), d_x - (i_midpoint * 1.0));
 
     ROS_INFO("roam_node] min_dist: %7.4f"
-             ", at x: %7.4f"
-             ", y: %7.4f"
+             ", at x: %ld (%7.4f)"
+             ", y: %ld (%7.4f)"
              ", angle: %7.4f"
-             , sqrt(min_dist)
-             , d_x
-             , d_y
-             , angle);
+             ", j_midpoint %ld, j_midpoint: %ld"
+             ", d_y - j_midpoint: %7.4f, d_x - i_midpoint: %7.4f"
+             , min_dist
+             , d_x, (d_x * costmap_msg->info.resolution) + costmap_msg->info.origin.position.x
+             , d_y, (d_y * costmap_msg->info.resolution) + costmap_msg->info.origin.position.y
+             , angle
+             , j_midpoint, i_midpoint
+             , d_y - (j_midpoint * 1.0), d_x - (i_midpoint * 1.0)
+    );
 }
 
 
